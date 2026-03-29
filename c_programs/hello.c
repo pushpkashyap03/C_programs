dbf97207756b95f560fcd6f459b54020dce5144a
pushpkashyap03 <pushpkashyap07@gmail.com>
2026-03-29 11:34:57 +0530

    added hello wprld program in basics

---
+++ c_programs/hello.c
@@ -0,0 +1,12 @@
+//..............................BASICS......................
+//.......WAP TO PRINT HELLO WORLD........
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter number :");
+    scanf("%d%d", &a, &b);
+    int sum = a+b;
+    printf("%d",sum);
+
+    return 0;
+}
