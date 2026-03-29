95561e0fda1e8e19d8e6416aad200b51d5a240ad
pushpkashyap03 <pushpkashyap07@gmail.com>
2026-03-29 20:45:46 +0530

    added a simple calculator

--- c_programs/hello.c
+++ c_programs/hello.c
@@ -1,25 +1,36 @@
 //..............................BASICS......................
-//.......WAP TO PRINT the largest of three numbers........
+//.......WAP TO make a simple calculator........
 #include<stdio.h>
 int main(){
-    int a,b,c;
-    printf("enter numbers :");
-    scanf("%d %d %d", &a,&b,&c);
+    int a, b, ch;
+    printf("addition : 1\n");
+    printf("substraction : 2\n");
+    printf("multiply : 3\n");
+    printf("division : 4\n");
+    printf("remainder : 5 \n");
+    printf("enter numbers :\n");
+    scanf("%d%d", &a, &b);
+    printf("enter choice :\n");
+    scanf("%d", &ch);
 
-    if(a>b){
-        if(a>c){
-            printf("%d is largest ",a);
-        }
-        else
-            printf("%d is largest",c);
-    }    
-    else if(b>a){
-        if(b>c){
-            printf("%d is largest",b);
-        }
-        else
-            printf("%d is largest",c);
+    int sum,subs,multi,div,rem;
+    if(ch==1){
+        printf("%d",sum = a+b);
     }
+    else if(ch==2){
+        printf("%d", subs = a-b);
+    }
+    else if(ch==3){
+        printf("%d", multi= a*b);
+    }
+    else if(ch==4){
+        printf("%d", div= a/b);
+    }
+    else if(ch==5){
+        printf("%d",rem=a%b);
+    }
+    else
+        printf("invalid choice");
 
     return 0;
 }
