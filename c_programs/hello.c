d80aa419cecfae8a752ee94ec19b45e6ba858f22
pushpkashyap03 <pushpkashyap07@gmail.com>
2026-03-29 21:01:11 +0530

    added the num from 1 to n

--- c_programs/hello.c
+++ c_programs/hello.c
@@ -1,36 +1,12 @@
-//..............................BASICS......................
-//.......WAP TO make a simple calculator........
+//.....write a program to print the number from 1 to n :..........
 #include<stdio.h>
 int main(){
-    int a, b, ch;
-    printf("addition : 1\n");
-    printf("substraction : 2\n");
-    printf("multiply : 3\n");
-    printf("division : 4\n");
-    printf("remainder : 5 \n");
-    printf("enter numbers :\n");
-    scanf("%d%d", &a, &b);
-    printf("enter choice :\n");
-    scanf("%d", &ch);
+    int n;
+    printf("enter n :");
+    scanf("%d", &n);
 
-    int sum,subs,multi,div,rem;
-    if(ch==1){
-        printf("%d",sum = a+b);
+    for(int i=1; i<=n; i++){
+        printf("%d\t", i);
     }
-    else if(ch==2){
-        printf("%d", subs = a-b);
-    }
-    else if(ch==3){
-        printf("%d", multi= a*b);
-    }
-    else if(ch==4){
-        printf("%d", div= a/b);
-    }
-    else if(ch==5){
-        printf("%d",rem=a%b);
-    }
-    else
-        printf("invalid choice");
-
     return 0;
 }
