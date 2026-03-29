16ce0362b9110331fc1db4438242c2141041618b
pushpkashyap03 <pushpkashyap07@gmail.com>
2026-03-29 11:42:25 +0530

    added swaping of two numbers numbers in basics

--- c_programs/hello.c
+++ c_programs/hello.c
@@ -1,12 +1,15 @@
 //..............................BASICS......................
-//.......WAP TO PRINT HELLO WORLD........
+//.......WAP TO PRINT the swaping of two numbers........
 #include<stdio.h>
 int main(){
     int a,b;
-    printf("enter number :");
-    scanf("%d%d", &a, &b);
-    int sum = a+b;
-    printf("%d",sum);
+    printf("enter two numbers ");
+    scanf("%d%d", &a,&b);
+    printf("before swaping a and b is : %d and %d \n",a, b);
+    int m=a;
+    a=b;
+    b=m;
+    printf("after swaping a and b : %d and %d ",a, b);
 
     return 0;
 }
