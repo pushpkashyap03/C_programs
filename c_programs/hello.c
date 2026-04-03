5e1dc1f108ff0b3e93c6a733da6bc88e8791c950
pushpkashyap03 <pushpkashyap07@gmail.com>
2026-04-03 21:30:12 +0530

    added the program to print sum and jump from zero and return sum

--- c_programs/hello.c
+++ c_programs/hello.c
@@ -1,16 +1,18 @@
 //.....WAP to Print numbers from 1 to n, and stop as soon as the num divisible by 20 encountered
 #include<stdio.h>
 int main(){
-    int n;
-    printf("enter numbers :");
+    int n, sum=0,num;
+    printf("how much numbers you want to perform :");
     scanf("%d", &n);
 
     for(int i=1; i<=n; i++){
-        if(i%20 == 0){
-            printf("__\n");
-            break;
+        printf("enter number %d :", i);
+        scanf("%d", &num);
+        if(num == 0){
+            continue;
         }
-    printf("%d\n", i);
+        sum += num;
     }
+    printf("the sum is : %d", sum);
     return 0;
 }
