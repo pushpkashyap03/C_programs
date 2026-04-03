64908f1a584b96eab841511c66ba1a695122406f
pushpkashyap03 <pushpkashyap07@gmail.com>
2026-04-03 08:16:30 +0530

    added to print the sum of the numbers that user wants

--- c_programs/hello.c
+++ c_programs/hello.c
@@ -2,29 +2,16 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-    int num,n=0,original,remainder;
-    double sum=0.0;
+    int n,num,sum=0;
+    printf("how much number you want to add ?\n");
+    scanf("%d", &n);
 
-    printf("enter number :");
-    scanf("%d", &num);
-    original=num;
+    for(int i=1; i<=n; i++){
+        printf("enter number %d :", i);
+        scanf("%d", &num);
+        sum += num;
+    }
+    printf("the total sum is : %d", sum);
 
-    int temp=original;
-    while(temp !=0 ){
-        temp /= 10;
-        n++;
-    }
-
-    temp = original;
-    while( temp != 0){
-        remainder = temp % 10;
-        sum += pow(remainder,n);
-        temp /= 10;
-    }
-    if((int)sum==original){
-        printf("%d is armstrong number ",original);
-    }
-    else 
-        printf("the num is not a armstrong num ");
     return 0;
 }
