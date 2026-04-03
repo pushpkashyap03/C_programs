1c0ade6aef428a2533b2bd8ff09145880dd04fc5
pushpkashyap03 <pushpkashyap07@gmail.com>
2026-04-03 08:37:57 +0530

    added to find the product of nums entered by user

--- c_programs/hello.c
+++ c_programs/hello.c
@@ -1,17 +1,25 @@
-//.....WAP to print the sum of the nums that the user wants to print
+//.....WAP to print the product of the nums that user wants
 #include<stdio.h>
 #include<math.h>
 int main(){
-    int n,num,sum=0;
-    printf("how much number you want to add ?\n");
+    int n, num, product=1;
+    printf("how much nums you want to multiply :");
     scanf("%d", &n);
 
     for(int i=1; i<=n; i++){
         printf("enter number %d :", i);
         scanf("%d", &num);
-        sum += num;
+
+        if(num!=0){
+            product *= num;
+        }
+        else if(num==0){
+            product=0; 
+            printf("%d", product);
+        }
+        else printf("");
     }
-    printf("the total sum is : %d", sum);
+    printf("the total product is : %d", product);
 
     return 0;
 }
