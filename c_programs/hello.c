a2b7d8b23288bafe6d48870ad42880b01527b9d8
pushpkashyap03 <pushpkashyap07@gmail.com>
2026-04-03 21:13:40 +0530

    added the program to jump to next num as the number divisible by 17 is encountered

--- c_programs/hello.c
+++ c_programs/hello.c
@@ -1,22 +1,16 @@
 //.....WAP to print prime number from 1 to n
 #include<stdio.h>
 int main(){
-    int n, isprime;
+    int n;
     printf("enter numbers :");
     scanf("%d", &n);
 
-    for(int i=2; i<=n; i++){
-        isprime = 1;
-        for(int j=2; j <= i/2; j++){
-            if(i % j == 0){
-                isprime = 0;
-                break;
-            }
+    for(int i=1; i<=n; i++){
+        if(i%17 == 0){
+            printf("__\n");
+            continue;
         }
-        if(isprime == 1){
-            printf("%d\n", i);
-        }
+    printf("%d\n", i);
     }
-
     return 0;
 }
